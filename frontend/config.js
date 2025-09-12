19a4f7483d8b46bdd805aef42eb55cd9d87a7c4a
sairaj2264 <sairaj.vadeghar@gmail.com>
2025-09-12 22:16:56 +0530

    Remove API base URL configuration from config.js

--- frontend/config.js
+++
@@ -1,6 +0,0 @@
-export const API_BASE_URL = "http://44.229.227.142:5000";
-// Backup URLs in case the primary is down
-export const BACKUP_URLS = [
-  "http://54.188.71.94:5000",
-  "http://52.13.128.108:5000",
-];
