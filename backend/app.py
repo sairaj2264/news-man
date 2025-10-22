627c3e1b14459611552118bad2e0432b01282983
Sai Raj Vadeghar <138302912+sairaj2264@users.noreply.github.com>
2025-10-22 23:48:02 +0530

    Update comment for application instance creation
    Clarify comment about creating the application instance.

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ load_dotenv()
 # Import the factory function from your 'app' package
 from app import create_app
 
-# Create the application instance by calling the factory
+# Create the application instance by calling the factory in the main
 app = create_app()
 
 if __name__ == '__main__':
